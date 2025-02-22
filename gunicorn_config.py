3e20acab7670d2ffa16917789712eafe6f5533bd
MZUHKHAN <fathepur78624@gmail.com>
2025-02-22 21:51:21 +0400

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -10,7 +10,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, current_dir)
 
 def main():
-    st.set_option('server.headless', True)
+    #st.set_option('server.headless', True)
     st.set_option('runner.installTracer', False)
 
     port = int(os.environ.get("PORT", 8501))
