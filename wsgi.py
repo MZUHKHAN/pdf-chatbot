93fc26c939a6ceaae65ce5eea5c444b503eb3032
MZUHKHAN <fathepur78624@gmail.com>
2025-02-22 18:46:35 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,15 @@
-import streamlit as st
-from streamlit.web import cli as stcli
-
-def main(*args):  
-    stcli.main_run()
-
-application = main
+import os
+    import streamlit as st
+    from streamlit.web import cli as stcli
+    
+    st.set_option('server.headless', True)  # Set to True for deployment
+    st.set_option('runner.installTracer', False)  # Disable tracer installation
+    
+    def main():
+        port = int(os.environ.get("PORT", 8501)) # Get port from environment or use default 8501
+        stcli.main_run(argv=["streamlit", "run", "app.py", "--server.port", str(port)]) 
+    
+    if __name__ == "__main__":
+        main()
+        
+    application = main
