8b721bdf8db2c63fea55f4bbba18f11f9dd134b9
MZUHKHAN <fathepur78624@gmail.com>
2025-02-22 21:34:13 +0400

    Update wsgl.py

--- wsgl.py
+++ wsgl.py
@@ -15,5 +15,27 @@ def main():
     port = int(os.environ.get("PORT", 8501))
     stcli.main_run(argv=["streamlit", "run", "app.py", "--server.port", str(port)])
 
+    st.set_page_config(page_title="Chat with Multiple PDF", layout="wide")
+    st.header("Chat with Multiple PDFs using Gemini")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files", type="pdf", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            if pdf_docs:
+                with st.spinner("Processing..."):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Done")
+            else:
+                st.warning("Please upload at least one PDF file.")
+
+
 if __name__ == "__main__":  # This ensures main is only called when the script is run directly
     main()
