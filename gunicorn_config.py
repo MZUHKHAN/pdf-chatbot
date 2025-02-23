4eb16ee9ef9c07d62e2671c43049c52544522138
MZUHKHAN <fathepur78624@gmail.com>
2025-02-23 14:35:18 +0400

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,14 +2,12 @@ import os
 from streamlit.web import cli as stcli
 from app import streamlit_app  # Import the streamlit_app function from app.py
 
-def main(environ, start_response):
-   
-    # Call the streamlit_app function to start the Streamlit app
-    streamlit_app()
-   
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    # start_response('200 OK', [('Content-Type', 'text/plain')]) # this was the original line for reference.
-    return [b'Hello World']  # Streamlit handles rendering; no need to return "Hello World"
+def main():
+    """
+    WSGI entry point for the Streamlit app.
+    """
+    streamlit_app()  # Call the streamlit_app function to start the Streamlit app
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8501))
